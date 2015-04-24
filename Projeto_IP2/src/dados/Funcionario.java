05fafb3c6a149e81ac6812ea18633c80850255ad
IkaroAlef <ikaroalef@gmail.com>
2015-04-24 15:14:22 -0300

    Primeiro Commit

---
+++ Projeto_IP2/src/dados/Funcionario.java
@@ -0,0 +1,13 @@
+package dados;
+
+public class Funcionario {
+	private String nome;
+	private String cpf;
+	private String email;
+	private String empresa;
+	private String cargo;
+	private String senha;
+	private String escala;
+	// qual tipo usar pra foto?
+	//falta horario
+}
