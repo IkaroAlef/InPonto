af73068116ad5494dabb04071705ae2c10448a78
IkaroAlef <ikaroalef@gmail.com>
2015-07-02 23:44:44 -0300

    teste

--- Projeto_IP2/src/gui/FrameLogin.java
+++ Projeto_IP2/src/gui/FrameLogin.java
@@ -13,6 +13,8 @@ import javax.swing.JTextField;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -143,5 +145,5 @@ public class FrameLogin extends JFrame implements ActionListener, KeyListener {
 	public void keyTyped(KeyEvent e) {
 
 	}
-	
+
 }
