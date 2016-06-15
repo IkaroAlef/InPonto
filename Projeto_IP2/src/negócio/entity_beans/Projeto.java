782cfe747851fb46bfc740465a98be9172d80ffe
wallsmanBR <alves_thiagobastos@hotmail.com>
2016-06-15 13:06:37 -0300

    Commit inicial da Class Projeto

---
+++ Projeto_IP2/src/negócio/entity_beans/Projeto.java
@@ -0,0 +1,14 @@
+package negócio.entity_beans;
+
+import javax.xml.crypto.Data;
+
+public class Projeto {
+	private int codigo;
+	private int horas;
+	private Data dataInicio;
+	private Data dataFim;
+	private Tarefa tarefa;
+	private Coordenador coordenador;
+	
+	
+}
